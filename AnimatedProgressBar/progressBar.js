fa6cd60c2aa537205782d46db2a238313a544d46
Santiago <ttsbpn@gmail.com>
2020-09-15 22:37:32 -0500

    add setTimeouts

--- AnimatedProgressBar/progressBar.js
+++ AnimatedProgressBar/progressBar.js
@@ -8,4 +8,8 @@ const changeProgress = progress => {
 // change progress after 1 second
 
 setTimeout(() => changeProgress(22), 10000);
-
+setTimeout(() => changeProgress(46), 10000);
+setTimeout(() => changeProgress(60), 10000);
+setTimeout(() => changeProgress(80), 10000);
+setTimeout(() => changeProgress(96), 10000);
+setTimeout(() => changeProgress(100), 10000);
