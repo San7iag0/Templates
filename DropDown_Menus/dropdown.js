cd52c63afbaea1e3c733899fd676f3658a0635fa
Santiago <ttsbpn@gmail.com>
2020-10-08 14:39:42 -0500

    DropDown Menu

---
+++ DropDown_Menus/dropdown.js
@@ -0,0 +1,5 @@
+// document.querySelectorAll('.dropdownContainer').forEach(Element => {
+//     Element.addEventListener('click', event => {
+//         event.target.classList.toggle('active');
+//     });
+// });
